fdd83fc251c30a246270b2d61cd16f08884be2e0
Yohai <151002392+Yohairra@users.noreply.github.com>
2024-07-08 16:39:53 +0800

    Add files via upload

---
+++ PuppersDAA.java
@@ -0,0 +1,12 @@
+package puppersdaa;
+
+public class PuppersDAA {
+        
+    public static void main(String[] args) {
+        // TODO code application logic here
+        LandingPage landingPage = new LandingPage();
+        landingPage.setVisible(true);
+        landingPage.setLocationRelativeTo(null);
+    }
+    
+}
